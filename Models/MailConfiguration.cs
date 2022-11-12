a0ffc7eb931dd2536533ec2976014e3cce5787b7
Udhayarajan J <udhayarajan080@gmail.com>
2022-11-12 21:34:34 +0530

    Mail Configuration Model Class Created

---
+++ Models/MailConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net5_job_schedule_with_email.Models
+{
+    public class MailConfiguration
+    {
+        public string name { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string host { get; set; }
+        public int port { get; set; }
+    }
+}
