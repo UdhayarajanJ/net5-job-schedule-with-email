aada0d04b3e3738573572643e61f658be7c7b66b
Udhayarajan J <udhayarajan080@gmail.com>
2022-11-12 20:52:20 +0530

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace net5_job_schedule_with_email
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
